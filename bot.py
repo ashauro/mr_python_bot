b9e7980f339e14e30fba4de3f091d9a52c65cdd6
Alexey Shauro <a.shauro@gmail.com>
2018-09-13 15:18:37 +0300

    bot with planets

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
+import datetime
 
 # Logging configuration
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -10,19 +12,46 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 
 def greet_user(bot, update):
     user_first_name = update.message.chat.first_name
-    text = "Привет, " + user_first_name + "! С помощью бота ты можешь вызывать команды /start и /howareyou, а также получать ответы на свои сообщения"
+    text = 'Привет, {}! С помощью бота ты можешь вызывать ' \
+           'команды /start, /howareyou и /planet (Марс, Венера, Сатурн), а также получать ответы ' \
+           'на свои сообщения'.format(user_first_name)
     logging.info('Вызван /start')
     update.message.reply_text(text)
 
-def today(bot, update):
+def mood(bot, update):
     text = 'ЗБС!'
     logging.info(text)
     update.message.reply_text(text)
 
+def planet(bot, update):
+    splitted_string = update.message.text.split(' ')
+    try:
+        planet_name = str(splitted_string[1]).lower()
+
+        logging.info ('Пользователь ввел: {}'.format(planet_name))
+
+        today_date = datetime.datetime.today()
+        us_date = today_date.strftime('%Y/%m/%d')
+
+        if planet_name == 'марс':
+            mars_today = ephem.Mars (us_date)
+            update.message.reply_text ('Марс:  {}'.format(ephem.constellation(mars_today)))
+        elif planet_name == 'венера':
+            venus_today = ephem.Venus (us_date)
+            update.message.reply_text ('Венера:  {}'.format(ephem.constellation(venus_today)))
+        elif planet_name == 'сатурн':
+            saturn_today = ephem.Saturn (us_date)
+            update.message.reply_text ('Сатурн:  {}'.format(ephem.constellation(saturn_today)))
+        else:
+            update.message.reply_text('У меня нет данных :(')
+    except IndexError:
+        update.message.reply_text('Пожалуйста, введите название планеты! Например /planet Марс')
+        logging.info ('Пользователь не указал планету')
+
 def talk_to_me (bot, update):
     user_first_name = update.message.chat.first_name
     user_text = update.message.text
-    user_reply = "Привет {}! Ты написал: {}".format(user_first_name, update.message.text)
+    user_reply = "Привет {}! Ты написал: {}".format(user_first_name, user_text)
     logging.info('Пользователь ' + user_first_name + ' написал в чат: ' + user_text)
     update.message.reply_text(user_reply)
 
@@ -33,12 +62,13 @@ def main ():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start",greet_user))
-    dp.add_handler(CommandHandler("howareyou", today))
+    dp.add_handler(CommandHandler("howareyou", mood))
+    dp.add_handler(CommandHandler("planet", planet))
     dp.add_handler(MessageHandler(Filters.text,talk_to_me))
 
     mybot.start_polling()
     mybot.idle()
 
 
-
-main()
+if __name__ == '__main__':
+    main()
